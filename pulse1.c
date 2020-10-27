850becc6265acb31b74a9faa628630771e2c1e9a
kurian <kurian@tre.local>
2020-10-27 20:12:13 +0530

    final streak
        - temperature
        - pulse

--- pulse1.c
+++ pulse1.c
@@ -5,12 +5,18 @@
 #include "interrupt.h"
 #define USEC_TO_SEC 		1000000
 #define CONV_FACTOR            0.00244140625
+#define PULSE_THRESHOLD  70; // threshold value for pulse
+#define PULSE_INIT   60;     // initial value for pulse
+#define TEMP_THRESHOLD 7;   // threshold value for pulse(0-10)
+#define TEMP_INIT  60;       // initial value for temperature
 void main() 
 {
 	UI adc_data, temp_data = 0;
     clock_t startTime, endTime;
     long int totaltime =0;
     int pulsecount, rate =0;
+    int temp_average=TEMP_INIT,pulse_average=PULSE_INIT; //intializing temperature and pulse average values
+    int counter=1;
     float adc_out, temp_out=0;
          startTime = read_csr(mcycle);
          endTime   = read_csr(mcycle);
@@ -31,7 +37,25 @@ void main()
 	  endTime  =  read_csr(mcycle);
 	  pulsecount =0;
   	  printf("\rPulse rate is %d", rate);
+	  counter++;
+	  temp_average=(temp_data+temp_average)/counter;
+	  pulse_average=(pulse_data+pulse_average)/counter;
+
+	  if (temp_average>TEMP_THRESHOLD){
+	    // set temp alert;
+	     on_LED(PIN_16);
+	   }else{
+	     off_LED(PIN_16);
+	   }
+	  if (pulse_average>PULSE_THRESHOLD){
+	    // set pulse alert;
+	      on_LED(PIN_17);
+	  }else{
+	      off_LED(PIN_17);
+	  }
+
        }
+
  
        adc_data = adc_analogRead(A0);
        temp_data= adc_analogRead(A1);
